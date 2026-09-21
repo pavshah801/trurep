a2b4eef9bcd59e7f562773294f575539914f0b17
pavshah801 <75145524+pavshah801@users.noreply.github.com>
2026-09-21 14:04:10 +0300

    Update trulux_appcalib.py

--- trulux_appcalib.py
+++ trulux_appcalib.py
@@ -33,6 +33,11 @@ BRND='Rolex'
 from selenium import webdriver
 from selenium.webdriver import FirefoxOptions
 from selenium.webdriver.common.by import By
+
+opts = FirefoxOptions()
+opts.add_argument("--headless")
+browser = webdriver.Firefox(options=opts)
+
 url='https://calibercorner.com/shop/'
 session = requests.Session()
 # send a get request to the server
